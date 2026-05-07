43c7d6ce39d1f540c9cdc329bb15dd3ef4f21b90
purva_28 <purvakhaire2005@gmail.com>
2026-05-07 18:39:49 +0530

    Add factorial calculation in factorial.java

---
+++ factorial.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class factorial {
+
+    // Function to calculate factorial
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int ans = factorial(num);
+
+        System.out.println("Factorial of " + num + " is: " + ans);
+
+        sc.close();
+    }
+}
